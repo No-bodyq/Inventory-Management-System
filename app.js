5a448b49e402041a8d9854c3101ecaebb074a5ea
Asher Nzurum <theforce289@gmail.com>
2024-11-16 15:58:53 +0100

    feat: add product controller and route

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 import customerRouter from "./routes/customerRoutes.js";
+import productRouter from "./routes/productRoutes.js";
 
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(dirname(fileURLToPath(import.meta.url)));
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.get("/", (req, res) => {
   res.status(200).json({
@@ -17,5 +25,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1/customer", customerRouter);
+app.use("/api/v1/product", productRouter);
 
 export default app;
--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,11 +1,11 @@
 import db from "../config/db.js";
 
+await db.connect();
+
 // Add a new customer
 const addCustomer = async (req, res) => {
   const { name, email, phone, address } = req.body;
   try {
-    await db.connect();
-
     const result = await db.query(
       "INSERT INTO Customers (name, email, phone, address) VALUES ($1, $2, $3, $4) RETURNING *",
       [name, email, phone, address]
@@ -16,35 +16,27 @@ const addCustomer = async (req, res) => {
   } catch (error) {
     console.error("Error adding customer:", error);
     res.status(500).json({ error: "Error adding customer" });
-  } finally {
-    db.end();
   }
 };
 
 // Get all customers
 const getAllCustomers = async (req, res) => {
   try {
-    await db.connect();
-
     const result = await db.query("SELECT * FROM Customers");
     res.status(200).json(result.rows);
   } catch (error) {
     console.error("Error fetching customers:", error);
     res.status(500).json({ error: "Error fetching customers" });
-  } finally {
-    db.end();
   }
 };
 
 // Get a customer by ID
 const getCustomerById = async (req, res) => {
-  const { customerId } = req.params;
+  const { id } = req.params;
   try {
-    await db.connect();
-
     const result = await db.query(
       "SELECT * FROM Customers WHERE customer_id = $1",
-      [customerId]
+      [id]
     );
     if (result.rows.length === 0) {
       return res.status(404).json({ message: "Customer not found" });
@@ -53,21 +45,17 @@ const getCustomerById = async (req, res) => {
   } catch (error) {
     console.error("Error fetching customer:", error);
     res.status(500).json({ error: "Error fetching customer" });
-  } finally {
-    db.end();
   }
 };
 
 // Update a customer's information
 const updateCustomer = async (req, res) => {
-  const { customerId } = req.params;
+  const { id } = req.params;
   const { name, email, phone, address } = req.body;
   try {
-    await db.connect();
-
     const result = await db.query(
       "UPDATE Customers SET name = $1, email = $2, phone = $3, address = $4 WHERE customer_id = $5 RETURNING *",
-      [name, email, phone, address, customerId]
+      [name, email, phone, address, id]
     );
     if (result.rows.length === 0) {
       return res.status(404).json({ message: "Customer not found" });
@@ -78,20 +66,16 @@ const updateCustomer = async (req, res) => {
   } catch (error) {
     console.error("Error updating customer:", error);
     res.status(500).json({ error: "Error updating customer" });
-  } finally {
-    db.end();
   }
 };
 
 // Delete a customer
 const deleteCustomer = async (req, res) => {
-  const { customerId } = req.params;
+  const { id } = req.params;
   try {
-    await db.connect();
-
     const result = await db.query(
       "DELETE FROM Customers WHERE customer_id = $1 RETURNING *",
-      [customerId]
+      [id]
     );
     if (result.rows.length === 0) {
       return res.status(404).json({ message: "Customer not found" });
@@ -102,8 +86,6 @@ const deleteCustomer = async (req, res) => {
   } catch (error) {
     console.error("Error deleting customer:", error);
     res.status(500).json({ error: "Error deleting customer" });
-  } finally {
-    db.end();
   }
 };
 
