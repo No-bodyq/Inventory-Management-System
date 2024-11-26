ca2ee4b29a1d501cfe333e998c52865fbbd86e88
Asher Nzurum <theforce289@gmail.com>
2024-11-26 02:39:20 +0100

    fix schema

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE Users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL,
+    password TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
